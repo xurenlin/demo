9e06c788fcc71dd18c6f42f1887c8629f776d8f6
xurenlin <1774091872@qq.com>
2020-07-09 13:49:05 +0800

    Initial Commit

---
+++ src/test/java/com/xrl/springdemo/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xrl.springdemo.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
